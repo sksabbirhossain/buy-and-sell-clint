15d24155b5fc06ddb90718ee8cd3c102f187bbb4
sksabbirhossain <sksabbirhossain4@gmail.com>
2022-11-24 11:03:30 +0600

    created a home page slider

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,10 +1,11 @@
 import React from "react";
+import Slider from "../../components/Slider/Slider";
 
 const Home = () => {
   return (
-    <div>
-      <h2 className="btn btn-success">home page</h2>
-    </div>
+    <>
+      <Slider />
+    </>
   );
 };
 

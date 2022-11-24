e1e09614e09df9b5e894ad89b96dd831536ebd98
sksabbirhossain <sksabbirhossain4@gmail.com>
2022-11-24 13:27:36 +0600

    created a user and save data in firebase

--- src/index.js
+++ src/index.js
@@ -3,14 +3,17 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import App from "./App";
+import { AuthProvider } from "./contexts/AuthContext";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <AuthProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </AuthProvider>
 );
 
 reportWebVitals();
--- src/pages/Signup/Signup.js
+++ src/pages/Signup/Signup.js
@@ -1,4 +1,4 @@
-// import { updateProfile } from "firebase/auth";
+import { updateProfile } from "firebase/auth";
 import React, { useState } from "react";
 import toast from "react-hot-toast";
 import { FaArrowRight } from "react-icons/fa";
@@ -6,72 +6,62 @@ import { Link, useNavigate } from "react-router-dom";
 import Button from "../../components/Button/Button";
 import Form from "../../components/Form/Form";
 import FormInput from "../../components/FormInput/FormInput";
-// import Spinner from "../../components/Spinner/Spinner";
-// import { useAuth } from "../contexts/AuthContext";
+import { useAuth } from "../../contexts/AuthContext";
 import styles from "../../styles/Signup.module.css";
-// import { dynamicTitle } from "../utilities/dynamicTitle";
 
 const Signup = () => {
   const [error, setError] = useState("");
   const [username, setUsername] = useState("");
-  const [photourl, setPhotourl] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
   const [loading, setLoading] = useState(false);
-  //   const { userSignup, googleSignin } = useAuth();
+  const { userSignup, googleSignin } = useAuth();
   const navigate = useNavigate();
 
   // form handle and signup function
-  //   const handleUserSignup = (e) => {
-  //     setLoading(true)
-  //     e.preventDefault();
-  //     //   validation
-  //     if (password !== confirmPassword) {
-  //       setLoading(false)
-  //       return setError("password not match");
-  //     }
-  //     userSignup(email, password, username, photourl)
-  //       .then((userInfo) => {
-  //         const user = userInfo.user;
-  //         console.log(user);
-  //         toast.success("User Create Successfull");
+  const handleUserSignup = (e) => {
+    setLoading(true);
+    e.preventDefault();
+    //   validation
+    if (password !== confirmPassword) {
+      setLoading(false);
+      return setError("password not match");
+    }
+    userSignup(email, password, username)
+      .then((userInfo) => {
+        const user = userInfo.user;
+        console.log(user);
+        toast.success("User Create Successfull");
 
-  //         // update profile
-  //         updateProfile(user, {
-  //           displayName: username,
-  //           photoURL: photourl,
-  //         });
-  //         setLoading(false)
-  //         navigate("/");
-  //       })
-  //       .catch((error) => {
-  //         setLoading(false)
-  //         console.log(error);
-  //         setError(error.message);
-  //       });
-  //   };
+        // update profile
+        updateProfile(user, {
+          displayName: username,
+        });
+        setLoading(false);
+        navigate("/");
+      })
+      .catch((error) => {
+        setLoading(false);
+        console.log(error);
+        setError(error.message);
+      });
+  };
 
-  //   if (loading) {
-  //     return <Spinner />;
-  //   }
   //   // google signup function
-  //   const handleGoogleSignup = () => {
-  //     googleSignin()
-  //       .then((result) => {
-  //         const user = result.user;
-  //         console.log(user);
-  //         toast.success("Google SignUp successful");
-  //         navigate("/");
-  //       })
-  //       .catch((error) => {
-  //         const errorMessage = error.message;
-  //         toast.error(errorMessage);
-  //       });
-  //   };
-
-  // add title
-  //   dynamicTitle("Signup page");
+  const handleGoogleSignup = () => {
+    googleSignin()
+      .then((result) => {
+        const user = result.user;
+        console.log(user);
+        toast.success("Google SignUp successful");
+        navigate("/");
+      })
+      .catch((error) => {
+        const errorMessage = error.message;
+        toast.error(errorMessage);
+      });
+  };
 
   return (
     <section className="mt-4 mt-md-5 ">
@@ -83,7 +73,7 @@ const Signup = () => {
           <div className="row">
             <div className="col-md-4 offset-md-4">
               <div className="card p-3 mb-4 shadow">
-                <Form>
+                <Form onSubmit={handleUserSignup}>
                   <FormInput
                     label="User Name"
                     type="text"
@@ -137,7 +127,7 @@ const Signup = () => {
                 <span className="py-2 text-center">OR</span>
                 <div className="shadow py-3 text-center rounded">
                   <span
-                    // onClick={handleGoogleSignup}
+                    onClick={handleGoogleSignup}
                     className={styles.signupIcons}
                   >
                     Signup With Google
